6531f7ca83136aa8edb956dd240957047c550765
AkashSDas <aakashdas368@gmail.com>
2021-08-02 14:50:48 +0530

    provide UserContext in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import { SideNavbar } from "../components/common/navbar";
+import { UserContext } from "../lib/context";
+import { useUserData } from "../lib/hooks";
 import "../styles/styles.scss";
 
 function MyApp({ Component, pageProps }) {
+  const userData = useUserData();
+
   return (
-    <>
+    <UserContext.Provider value={userData}>
       <SideNavbar />
       <Component {...pageProps} />
-    </>
+    </UserContext.Provider>
   );
 }
 
