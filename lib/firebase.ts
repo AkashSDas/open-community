f40a631c37ce2911a685cb3359fe7b8a999fcf55
AkashSDas <aakashdas368@gmail.com>
2021-08-02 14:49:55 +0530

    add firestore global variable and export it from firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -18,3 +18,6 @@ if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
 // Auth exports
 export const auth = firebase.auth();
 export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+// Firestore exports
+export const firestore = firebase.firestore();
