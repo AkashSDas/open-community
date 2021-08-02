fe9b2a847b6cda9d8ebd38cf9015a18c3eb32333
AkashSDas <aakashdas368@gmail.com>
2021-08-02 08:38:55 +0530

    add navbar sections

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -1,7 +1,49 @@
+import CupSVG from "../svg_icons/cup";
+import EditSVG from "../svg_icons/edit";
 import HomeSVG from "../svg_icons/home";
 import LogoSVG from "../svg_icons/logo";
+import MenuSVG from "../svg_icons/menu";
+import PresentationSVG from "../svg_icons/presentation";
+import SavedSVG from "../svg_icons/saved";
+import SearchSVG from "../svg_icons/search";
+import SettingsSVG from "../svg_icons/settings";
+import ShoppingCartSVG from "../svg_icons/shopping_cart";
+import UserSVG from "../svg_icons/user";
+import UserPlusSVG from "../svg_icons/user_plus";
+
+class NavbarSectionItem {
+  icon: JSX.Element;
+  text: string;
+  active: boolean;
+
+  constructor(icon: JSX.Element, text: string, active: boolean) {
+    this.icon = icon;
+    this.text = text;
+    this.active = active;
+  }
+}
 
 export function SideNavbar() {
+  const section1: NavbarSectionItem[] = [
+    new NavbarSectionItem(<HomeSVG />, "Home", false),
+    new NavbarSectionItem(<PresentationSVG />, "Trending", false),
+    new NavbarSectionItem(<MenuSVG />, "Topics", false),
+    new NavbarSectionItem(<SearchSVG />, "Search", false),
+    new NavbarSectionItem(<EditSVG />, "Write", false),
+    new NavbarSectionItem(<SavedSVG />, "Bookmark", false),
+  ];
+
+  const section2: NavbarSectionItem[] = [
+    new NavbarSectionItem(<UserSVG />, "Admin", false),
+    new NavbarSectionItem(<SettingsSVG />, "Settings", false),
+  ];
+
+  const section3: NavbarSectionItem[] = [
+    new NavbarSectionItem(<CupSVG />, "Earn", false),
+    new NavbarSectionItem(<ShoppingCartSVG />, "Upgrade", false),
+    new NavbarSectionItem(<UserPlusSVG />, "Login", false),
+  ];
+
   return (
     <nav className="navbar">
       <NavSection>
@@ -10,10 +52,37 @@ export function SideNavbar() {
         </NavItem>
       </NavSection>
       <NavSection>
-        <NavItem>
-          <NavbarBtn icon={<HomeSVG />} text="Home" active={false} />
-          <NavbarBtn icon={<HomeSVG />} text="Home" active={true} />
-        </NavItem>
+        {section1.map((value: NavbarSectionItem, key: number) => (
+          <NavItem key={key}>
+            <NavbarBtn
+              icon={value.icon}
+              text={value.text}
+              active={value.active}
+            />
+          </NavItem>
+        ))}
+      </NavSection>
+      <NavSection>
+        {section2.map((value: NavbarSectionItem, key: number) => (
+          <NavItem key={key}>
+            <NavbarBtn
+              icon={value.icon}
+              text={value.text}
+              active={value.active}
+            />
+          </NavItem>
+        ))}
+      </NavSection>
+      <NavSection>
+        {section3.map((value: NavbarSectionItem, key: number) => (
+          <NavItem key={key}>
+            <NavbarBtn
+              icon={value.icon}
+              text={value.text}
+              active={value.active}
+            />
+          </NavItem>
+        ))}
       </NavSection>
     </nav>
   );
