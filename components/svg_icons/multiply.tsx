d4dc98f8ea08683a196e66f1af9c52fe17d07eea
AkashSDas <aakashdas368@gmail.com>
2021-08-03 20:50:14 +0530

    add multiply.tsx

---
+++ components/svg_icons/multiply.tsx
@@ -0,0 +1,20 @@
+function MultiplySVG() {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M9.17523 14.8285L14.8321 9.1716M9.17523 9.1716L14.8321 14.8285"
+        stroke="white"
+        stroke-width="1.5"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+}
+
+export default MultiplySVG;
