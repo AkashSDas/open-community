a28ae20dbc1af0c2dfee58340329abc4b69b29a5
AkashSDas <aakashdas368@gmail.com>
2021-08-04 09:45:03 +0530

    write note in useResizeTextareaHeight

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -29,6 +29,17 @@ export function useUserData() {
 }
 
 /// Resize textarea height dynamically
+///
+/// NOTE: If you've a case where your textarea is mounting & unmounting
+/// but not your main component where you've defined this hook then extract
+/// textarea into separate hook and that way you can use the lifecycle
+/// events (mounting and unmounting) of the textarea and not the top component
+/// where this hook is defined. If you don't do this then textare if
+/// having default `value` will have height that'll fit the content but as
+/// the textarea is unmounted (not the top component where this hook is defined)
+/// and then mounted again, now the height will be default textarea height and
+/// this might not fit your content and then you've to scroll.
+/// Similar situation was faced in `editor-mode-0` in post_form.tsx
 export function useResizeTextareaHeight(value: string) {
   const ref = useRef(null);
 
