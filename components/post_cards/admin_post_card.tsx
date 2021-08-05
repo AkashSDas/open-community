780ce5c49705caf9afa864632817811745f37f33
AkashSDas <aakashdas368@gmail.com>
2021-08-05 17:57:48 +0530

    extract admin post cards and style it

--- components/post_cards/admin_post_card.tsx
+++ components/post_cards/admin_post_card.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { auth, firestore, postToJSON } from "../../lib/firebase";
 import { useUserData } from "../../lib/hooks";
+import ShowSVG from "../svg_icons/show";
 
 // Max post to query per page
 const LIMIT = 10;
@@ -10,53 +11,123 @@ function AdminPostCard(props) {
   const { user } = useUserData();
   const [posts, setPosts] = useState([]);
 
-  if (!user) return <main>Loading...</main>;
-
   const getPost = async () => {
     const postQuery = firestore
       .collection("/posts")
       .where("authorId", "==", user.uid)
+      .orderBy("lastmodifiedAt", "desc")
       .limit(LIMIT);
 
     const postsData = await postQuery.get();
-
+    const postList = [];
+    let count = 0;
     postsData.docs.map(async (doc) => {
-      const data = doc.data();
+      let data = doc.data();
+      console.log(data);
+      // firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+      data = {
+        ...data,
+        createdAt: data?.createdAt?.toMillis() || 0,
+        lastmodifiedAt: data?.lastmodifiedAt?.toMillis() || 0,
+      };
 
       const metadataDoc = firestore.doc(`postMetadata/${data.metadataId}`);
       const metadataData = (await metadataDoc.get()).data();
+
       const postData = {
         ...data,
-        metadataData,
+        ...metadataData,
       };
 
-      setPosts((postList) => [...postList, postData]);
+      postList.push(postData);
+
+      /// to make setPosts run only when we have iterated through
+      /// entire docs
+      if (count === postsData.docs.length - 1) {
+        setPosts(postList);
+        count = 0;
+      } else {
+        count++;
+      }
     });
   };
 
-  //   getPost();
+  useEffect(() => {
+    if (user) {
+      (async () => await getPost())();
+    }
+  }, [user]);
+
+  if (!user) return <main>Loading...</main>;
 
   return (
-    <section>
+    <section className="admin-posts">
       <div className="heading">Your Posts</div>
-      <hr />
+      <div className="hr"></div>
 
-      {JSON.stringify(posts)}
+      {posts && posts.map((post, key) => <Card key={key} post={post} />)}
     </section>
   );
 }
 
-// export async function getServerSideProps(context) {
-//   const postQuery = firestore
-//     .collection("/posts")
-//     .where("authorId", "==", auth.currentUser.uid)
-//     .limit(LIMIT);
+function Card({ post }) {
+  const convertSecToJsxTime = (time) => {
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
 
-//   const posts = await postQuery.get();
+    const date = new Date(time);
+    return `${date.getDay()} ${monthNames[date.getMonth()].slice(
+      0,
+      3
+    )}, ${date.getFullYear()}`;
+  };
 
-//   return {
-//     props: { posts },
-//   };
-// }
+  return (
+    <div className="admin-post-card">
+      <img className="cover-img" src={`${post.coverImgURL}`} />
+      <div className="info">
+        <h4>{post.title}</h4>
+        <div className="description">{post.description}</div>
+        <div className="other-info">
+          <span>
+            <span>{convertSecToJsxTime(post.lastmodifiedAt)}</span>
+            {post.publish ? <span className="space">-</span> : null}
+            {post.publish ? (
+              <span className="views">
+                <ShowSVG /> {post.views}
+              </span>
+            ) : null}
+          </span>
+
+          <span className="action-btns">
+            {post.publish ? (
+              <span>
+                <button>Publish</button>
+              </span>
+            ) : null}
+            <span>
+              <button>Edit</button>
+            </span>
+            <span>
+              <button>Delete</button>
+            </span>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default AdminPostCard;
