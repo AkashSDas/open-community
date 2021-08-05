d5cf7a22e979ade30eac4e298a8584226b86719b
AkashSDas <aakashdas368@gmail.com>
2021-08-05 12:20:39 +0530

    extract creating post to WriteManager component and then create create post just via title and then edit that post in /edit

---
+++ pages/edit.tsx
@@ -0,0 +1,129 @@
+import { Formik } from "formik";
+import { useRouter, withRouter } from "next/router";
+import React, { useState } from "react";
+import { useCallback } from "react";
+import { useEffect } from "react";
+import PostForm from "../components/write/post_form";
+import { useDocumentDataOnce } from "react-firebase-hooks/firestore";
+import { auth, firestore, serverTimestamp } from "../lib/firebase";
+import { IPost, IPostMetadataThatAuthorCanSet } from "./write";
+
+function Edit() {
+  return <WriteManager />;
+}
+
+function WriteManager() {
+  const router = useRouter();
+  const [postState, setPostState] = useState({
+    post: null,
+    metadata: null,
+    content: null,
+  });
+
+  (async () => {
+    if (router.query?.postId) {
+      const postDocRef = firestore.doc(`posts/${router.query.postId}`);
+      const post = await postDocRef.get();
+      const postData = post.data();
+
+      const metadataDocRef = firestore.doc(
+        `postMetadata/${postData.metadataId}`
+      );
+      const metadata = await metadataDocRef.get();
+
+      const postContentRef = firestore.doc(
+        `postContents/${postData.postContentId}`
+      );
+      const postContent = await postContentRef.get();
+
+      setPostState({
+        post: post,
+        metadata: metadata,
+        content: postContent,
+      });
+    }
+  })();
+
+  if (
+    postState.content !== null &&
+    postState.metadata !== null &&
+    postState.post !== null
+  ) {
+    return <PostEditForm postState={postState} />;
+  }
+
+  return <main>Loading...</main>;
+}
+
+function PostEditForm({ postState: { metadata, post, content } }) {
+  const metadataData = metadata.data();
+  const contentData = content.data();
+  const postData = post.data();
+
+  const [tags, setTags] = useState<string[]>(metadataData.tags);
+  const [publish, setPublish] = useState(postData.publish);
+  const [postMetadata, setPostMetadata] =
+    useState<IPostMetadataThatAuthorCanSet>({
+      readTime: metadataData.readTime,
+      numOfWords: metadataData.wordCount,
+    });
+
+  const onSubmit = async (values: IPost) => {
+    const batch = firestore.batch();
+
+    const postRef = firestore.doc(`posts/${post.id}`);
+    const metadataRef = firestore.doc(`postMetadata/${metadata.id}`);
+    const contentRef = firestore.doc(`postContents/${content.id}`);
+
+    batch.update(postRef, {
+      coverImgURL: values.coverImageURL,
+      title: values.title,
+      description: values.description,
+      publish,
+    });
+
+    batch.update(metadataRef, {
+      lastmodifiedAt: serverTimestamp(),
+      numOfWords: postMetadata.numOfWords,
+      readTime: postMetadata.readTime,
+      tags,
+    });
+
+    batch.update(contentRef, {
+      content: values.content,
+    });
+
+    await batch.commit();
+  };
+
+  const initialFormValues: IPost = {
+    title: postData.title,
+    description: postData.description,
+    coverImageURL: postData.coverImgURL,
+    content: contentData.content,
+  };
+
+  return (
+    <main>
+      <section className="write-section">
+        <Formik onSubmit={onSubmit} initialValues={initialFormValues}>
+          {({ values, handleSubmit, handleChange }) => (
+            <PostForm
+              values={values}
+              handleSubmit={handleSubmit}
+              handleChange={handleChange}
+              tags={tags}
+              setTags={setTags}
+              publish={publish}
+              setPublish={setPublish}
+              postMetadata={postMetadata}
+              setPostMetadata={setPostMetadata}
+            />
+          )}
+        </Formik>
+      </section>
+    </main>
+  );
+}
+
+export default Edit;
--- pages/write.tsx
+++ pages/write.tsx
@@ -1,6 +1,6 @@
 import { Formik } from "formik";
+import { useRouter } from "next/router";
 import { FormEvent, useState } from "react";
-import PostForm from "../components/write/post_form";
 import { auth, firestore, serverTimestamp } from "../lib/firebase";
 
 export interface IPost {
@@ -16,12 +16,19 @@ export interface IPostMetadataThatAuthorCanSet {
 }
 
 function Write() {
-  const [tags, setTags] = useState<string[]>([]);
-  const [publish, setPublish] = useState(false);
-  const [postMetadata, setPostMetadata] =
-    useState<IPostMetadataThatAuthorCanSet>(null);
+  const [redirect, setRedirect] = useState({
+    doRedirect: false,
+    postDocId: null,
+  });
+  const router = useRouter();
 
-  const onSubmit = (values: IPost) => {
+  const performRedirect = () => {
+    if (redirect.doRedirect) {
+      router.push({ pathname: "/edit", query: { postId: redirect.postDocId } });
+    }
+  };
+
+  const onSubmit = async (values) => {
     // Commit both docs together as a batch write.
     const batch = firestore.batch();
     const authorId = auth.currentUser.uid;
@@ -31,61 +38,140 @@ function Write() {
       createdAt: serverTimestamp(),
       lastmodifiedAt: serverTimestamp(),
       hearts: 0,
-      numOfWords: postMetadata.numOfWords,
-      readTime: postMetadata.readTime,
-      tags: tags,
+      numOfWords: 0,
+      readTime: 0,
+      tags: [],
       views: 0,
     };
     batch.set(metadataDoc, metadataData);
 
     const postContentDoc = firestore.collection("postContents").doc();
     const postContentData = {
-      content: values.content,
+      content: "",
     };
     batch.set(postContentDoc, postContentData);
 
     const postDoc = firestore.collection("posts").doc();
     const postData = {
       authorId,
-      coverImgURL: values.coverImageURL,
+      coverImgURL: "",
       title: values.title,
-      description: values.description,
+      description: "",
       metadataId: metadataDoc.id,
       postContentId: postContentDoc.id,
+      publish: false,
     };
     batch.set(postDoc, postData);
 
-    batch.commit();
+    await batch.commit();
+
+    setRedirect({
+      doRedirect: true,
+      postDocId: postDoc.id,
+    });
   };
 
-  const initialFormValues: IPost = {
+  const initialFormValues: { title: string } = {
     title: "",
-    description: "",
-    coverImageURL: "",
-    content: "",
   };
 
   return (
     <main>
-      <section className="write-section">
-        <Formik onSubmit={onSubmit} initialValues={initialFormValues}>
-          {({ values, handleSubmit, handleChange }) => (
-            <PostForm
-              values={values}
-              handleSubmit={handleSubmit}
-              handleChange={handleChange}
-              tags={tags}
-              setTags={setTags}
-              publish={publish}
-              setPublish={setPublish}
-              postMetadata={postMetadata}
-              setPostMetadata={setPostMetadata}
-            />
+      <section>
+        <Formik initialValues={initialFormValues} onSubmit={onSubmit}>
+          {({ values, handleChange, handleSubmit }) => (
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="title"
+                value={values.title}
+                onChange={handleChange}
+                placeholder="Enter title for your post"
+              />
+
+              <button type="submit">Choose</button>
+            </form>
           )}
         </Formik>
+        {performRedirect()}
       </section>
     </main>
   );
 }
 
 export default Write;
+
+/// WriteManager (just for creating new post) ///
+
+// function WriteManager() {
+//   const [tags, setTags] = useState<string[]>([]);
+//   const [publish, setPublish] = useState(false);
+//   const [postMetadata, setPostMetadata] =
+//     useState<IPostMetadataThatAuthorCanSet>(null);
+
+//   const onSubmit = (values: IPost) => {
+//     // Commit both docs together as a batch write.
+//     const batch = firestore.batch();
+//     const authorId = auth.currentUser.uid;
+
+//     const metadataDoc = firestore.collection("postMetadata").doc();
+//     const metadataData = {
+//       createdAt: serverTimestamp(),
+//       lastmodifiedAt: serverTimestamp(),
+//       hearts: 0,
+//       numOfWords: postMetadata.numOfWords,
+//       readTime: postMetadata.readTime,
+//       tags: tags,
+//       views: 0,
+//     };
+//     batch.set(metadataDoc, metadataData);
+
+//     const postContentDoc = firestore.collection("postContents").doc();
+//     const postContentData = {
+//       content: values.content,
+//     };
+//     batch.set(postContentDoc, postContentData);
+
+//     const postDoc = firestore.collection("posts").doc();
+//     const postData = {
+//       authorId,
+//       coverImgURL: values.coverImageURL,
+//       title: values.title,
+//       description: values.description,
+//       metadataId: metadataDoc.id,
+//       postContentId: postContentDoc.id,
+//     };
+//     batch.set(postDoc, postData);
+
+//     batch.commit();
+//   };
+
+//   const initialFormValues: IPost = {
+//     title: "",
+//     description: "",
+//     coverImageURL: "",
+//     content: "",
+//   };
+
+//   return (
+//     <main>
+//       <section className="write-section">
+//         <Formik onSubmit={onSubmit} initialValues={initialFormValues}>
+//           {({ values, handleSubmit, handleChange }) => (
+//             <PostForm
+//               values={values}
+//               handleSubmit={handleSubmit}
+//               handleChange={handleChange}
+//               tags={tags}
+//               setTags={setTags}
+//               publish={publish}
+//               setPublish={setPublish}
+//               postMetadata={postMetadata}
+//               setPostMetadata={setPostMetadata}
+//             />
+//           )}
+//         </Formik>
+//       </section>
+//     </main>
+//   );
+// }
