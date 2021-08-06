857e202980d8afca153214db9a442177c9ad02ad
AkashSDas <aakashdas368@gmail.com>
2021-08-06 10:50:32 +0530

    refactor  posts in lib/ into a dir with index.ts and interfaces.ts

--- lib/posts/index.ts
+++ lib/posts/index.ts
@@ -1,41 +1,15 @@
+import { FirebaseUser, firestore, serverTimestamp } from "../firebase";
 import {
-  auth,
-  firestore,
-  FirestoreFieldValue,
-  FirestoreTimestamp,
-  serverTimestamp,
-} from "./firebase";
+  INewPostValues,
+  IPostContentsDoc,
+  IPostMetadataDoc,
+  IPostsDoc,
+} from "./interfaces";
 
-interface IPostMetadataDoc {
-  tags: string[];
-  numOfWords: number;
-  readTime: number;
-  hearts: number;
-  views: number;
-}
-
-interface IPostContentsDoc {
-  content: string;
-}
-
-interface IPostsDoc {
-  authorId: string;
-  title: string;
-  description: string;
-  coverImgURL: string;
-  createdAt: number | FirestoreTimestamp | FirestoreFieldValue;
-  lastmodifiedAt: number | FirestoreTimestamp | FirestoreFieldValue;
-  publish: boolean;
-  metadataId: string; // post metadata doc id
-  postContentId: string; // post content doc id
-}
-
-// values that are needed while creating new post
-interface INewPostValues {
-  title: string;
-}
-
-export async function createNewPost(values: INewPostValues) {
+export async function createNewPost(
+  values: INewPostValues,
+  user: FirebaseUser
+) {
   // Commit both docs together as a batch write.
   const batch = firestore.batch();
 
@@ -60,7 +34,7 @@ export async function createNewPost(values: INewPostValues) {
   // Post doc
   const postRef = firestore.collection("posts").doc();
   const postData: IPostsDoc = {
-    authorId: auth.currentUser.uid,
+    authorId: user.uid,
     title: values.title,
     description: "",
     coverImgURL: "",
@@ -76,5 +50,3 @@ export async function createNewPost(values: INewPostValues) {
 
   return { postRef };
 }
-
-export {};
---
+++ lib/posts/interfaces.ts
@@ -0,0 +1,34 @@
+import {
+  FirebaseUser,
+  FirestoreFieldValue,
+  FirestoreTimestamp,
+} from "../firebase";
+
+export interface IPostMetadataDoc {
+  tags: string[];
+  numOfWords: number;
+  readTime: number;
+  hearts: number;
+  views: number;
+}
+
+export interface IPostContentsDoc {
+  content: string;
+}
+
+export interface IPostsDoc {
+  authorId: string;
+  title: string;
+  description: string;
+  coverImgURL: string;
+  createdAt: number | FirestoreTimestamp | FirestoreFieldValue;
+  lastmodifiedAt: number | FirestoreTimestamp | FirestoreFieldValue;
+  publish: boolean;
+  metadataId: string; // post metadata doc id
+  postContentId: string; // post content doc id
+}
+
+// values that are needed while creating new post
+export interface INewPostValues {
+  title: string;
+}
