9d2aec2199d9e059e54efc71ccedd5f7073fb85d
AkashSDas <aakashdas368@gmail.com>
2021-08-07 11:01:23 +0530

    create components/common/greeting.tsx

---
+++ components/common/greeting.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+import SunriseSVG from "../svg_icons/sunrise";
+
+function Greeting() {
+  const [greeting, setGreeting] = useState(null);
+
+  useEffect(() => {
+    if (new Date(Date.now()).getHours() < 12) setGreeting("Good morning");
+  }, []);
+
+  const iconJsx = () => {
+    if (greeting) {
+      return <SunriseSVG />;
+    }
+  };
+
+  return (
+    <section className="datetime-greeting">
+      {iconJsx()} {greeting}
+    </section>
+  );
+}
+
+export default Greeting;
