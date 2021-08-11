6f7454c74243306909a550d006e95b55cec42916
AkashSDas <aakashdas368@gmail.com>
2021-08-11 10:50:53 +0530

    fetch intitial post data in server and then load more data on client side in pages/admin/[username].tsx

--- pages/admin/[username].tsx
+++ pages/admin/[username].tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
 
 import Divider from "../../components/common/divider";
@@ -6,20 +7,77 @@ import AuthorsToFollow from "../../components/home/authors_to_follow";
 import SmallPostCardtListView from "../../components/home/small_post_card_listview";
 import PostCard from "../../components/post_cards/post_card";
 import SmallPostCard from "../../components/post_cards/small_post_card";
+import {
+    firestore, FirestoreDocumentData, fromMillis, serializePostDocData
+} from "../../lib/firebase";
 import { useFollowingAuthors } from "../../lib/hooks/followings";
 import { useAuthorPostsDataOnce } from "../../lib/hooks/posts/author_posts";
+import { useAuthorMorePostsDataOnce } from "../../lib/hooks/posts/more_posts";
 
-function Admin() {
-  const router = useRouter();
-  const {
-    data,
-    pageLoading,
-    loadMoreLoading,
-    error,
-    author,
-    postEnds,
-    getData,
-  } = useAuthorPostsDataOnce(2);
+// Since the data for admin's or author's posts won't change that often
+// so this is a good candidate for traditional server side rendering
+// which will make it visible to SEO bots too
+
+export const getServerSideProps: GetServerSideProps = async ({
+  query,
+}): Promise<any> => {
+  const { username } = query;
+
+  // get user doc
+  const authorQuery = firestore
+    .collection("users")
+    .where("username", "==", username)
+    .limit(1);
+  const userDoc = (await authorQuery.get()).docs.map((doc) => {
+    return { id: doc.id, data: doc.data() };
+  });
+
+  if (userDoc.length === 0) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const user = userDoc[0];
+  const postQuery = firestore
+    .collection("posts")
+    .where("authorId", "==", user.id)
+    .orderBy("lastmodifiedAt", "desc")
+    .limit(2);
+  const posts = (await postQuery.get()).docs.map((doc) => {
+    return {
+      id: doc.id,
+      data: serializePostDocData(doc),
+    };
+  });
+
+  let data = [];
+  for await (const post of posts) {
+    const metadataQuery = firestore.doc(`/postMetadata/${post.id}`);
+    const metadataDoc = await metadataQuery.get();
+    data.push({ post, metadata: metadataDoc.data() });
+  }
+
+  return {
+    props: {
+      user,
+      data,
+    },
+  };
+};
+
+function Admin({ user: author, data }) {
+  // get posts data on client side (every thing is taken care of by the
+  // useAuthorPostDataOnce hook)
+  // const {
+  //   data,
+  //   pageLoading,
+  //   loadMoreLoading,
+  //   error,
+  //   author,
+  //   postEnds,
+  //   getData,
+  // } = useAuthorPostsDataOnce(2);
 
   const {
     followings,
@@ -27,6 +85,14 @@ function Admin() {
     error: followingsError,
     followAction,
   } = useFollowingAuthors();
+
+  const {
+    data: posts,
+    loading,
+    error,
+    getData,
+    postEnds,
+  } = useAuthorMorePostsDataOnce(author.id, data, 2);
 
   const checkFollowing = (authorId: string) => {
     if (followings && !followingsLoading && !followingsError) {
@@ -36,10 +102,6 @@ function Admin() {
     }
     return false;
   };
-
-  if (pageLoading || error) return <main>Loading...</main>;
-
-  if (!author) return <main>Author doesn't exists</main>;
 
   return (
     <main className="admin">
@@ -77,16 +139,16 @@ function Admin() {
           <SizedBox height="1rem" />
 
           <div className="posts-listview">
-            {data.map((d, key: number) => (
+            {posts.map((d, key: number) => (
               <>
                 <PostCard
                   key={key}
                   post={{
-                    id: d.id,
-                    coverImgURL: d.post.coverImgURL,
-                    title: d.post.title,
-                    description: d.post.description,
-                    lastmodifiedAt: d.post.lastmodifiedAt,
+                    id: d.post.id,
+                    coverImgURL: d.post.data.coverImgURL,
+                    title: d.post.data.title,
+                    description: d.post.data.description,
+                    lastmodifiedAt: d.post.data.lastmodifiedAt,
                     views: d.metadata.views,
                     readTime: d.metadata.readTime,
                   }}
@@ -95,11 +157,11 @@ function Admin() {
               </>
             ))}
 
-            {data.length !== 0 && !loadMoreLoading && !postEnds && (
-              <button onClick={() => getData(true)}>Load more</button>
+            {posts.length !== 0 && !loading && !postEnds && (
+              <button onClick={() => getData()}>Load more</button>
             )}
 
-            {loadMoreLoading && "Loading..."}
+            {loading && "Loading..."}
 
             {postEnds && "You have reached the end!"}
           </div>
