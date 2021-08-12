13a8c0d465555965b5ef6b5d3925eeab6c0b4a32
AkashSDas <aakashdas368@gmail.com>
2021-08-12 19:21:05 +0530

    create fetchFromAPI util function in utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -20,3 +20,45 @@ export const convertSecToJsxTime = (time: number) => {
     3
   )}, ${date.getFullYear()}`;
 };
+
+import { auth } from "./firebase";
+
+const API = process.env.NEXT_PUBLIC_BASE_URL;
+
+/**
+ * A helper function to fetch data from your API
+ */
+
+export async function fetchFromAPI(
+  endpointURL: string,
+  opts: {
+    method: string;
+    body: any;
+  }
+) {
+  const { method, body } = { ...opts };
+
+  /**
+   * Once the user is logged in, firebase creates a JWT with the
+   * user's details in the frontend
+   *
+   * We can authenticate request that goes to the backend API by
+   * setting this token as a header on the request
+   */
+
+  const user = auth.currentUser;
+  const token = user && (await user.getIdToken());
+
+  const res = await fetch(`${API}${endpointURL}`, {
+    method,
+    ...(body && { body: JSON.stringify(body) }),
+    headers: {
+      "Content-Type": "application/json",
+
+      // Below is the standard format for JWT authentication
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return res.json();
+}
